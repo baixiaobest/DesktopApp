cdf01e7202155042fdfabd1db5326b8f84499768
Baiyu Huang <baiyuhuang@Baiyus-MacBook-Pro.local>
2018-07-19 11:08:14 +0800

    without react

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ document.getElementById('compile').addEventListener("click", testClick);
 function testClick(){
     console.log("Click");
     shell.exec('echo Start compiling and uploading',{async:true}).stdout;
-    shell.exec('make -C ./arduino_files/test_led/ upload',{async:true}).stdout;
+    //shell.exec('make -C ./arduino_files/test_led/ ',{async:true}).stdout;
+    shell.exec('/Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C/Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf -v -pm328p -carduino -b115200  -P/dev/cu.usbmodem14141 -Uflash:w:./arduino_files/test_led/build-uno/test_led.hex:i -carduino',{async:true}).stdout;
     shell.exec('echo upload successfully',{async:true}).stdout;
 }

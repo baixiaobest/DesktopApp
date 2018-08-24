3cda77748af7b9998842a095a0a5b19c0a1fc26f
Baiyu Huang <baiyuhua@usc.edu>
2018-08-24 10:34:23 -0700

    select device and compile is working

--- public/index.js
+++ public/index.js
@@ -1,13 +1,16 @@
 var electron = require('electron');
 var fs = require('fs');
 var shell = require('shelljs');
-//const smalltalk = require('smalltalk/native');
 // shell.config.execPath = process.execPath;
 
+
 $("#compile").click(function(){
+    var deviceList = $("#devices_list")[0];
+    var deviceName = deviceList.options[deviceList.selectedIndex].value;
+    var compileCommand = '/Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C/Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf -v -pm328p -carduino -b115200  -P/dev/' + deviceName +' -Uflash:w:./arduino_files/test_led/build-uno/test_led.hex:i -carduino';
     shell.exec('echo Start compiling and uploading',{async:true}).stdout;
     shell.exec('make -C ./arduino_files/test_led/ ',{async:true}).stdout;
-    shell.exec('/Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C/Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf -v -pm328p -carduino -b115200  -P/dev/cu.usbserial-AI06B9PO -Uflash:w:./arduino_files/test_led/build-uno/test_led.hex:i -carduino',{async:true}).stdout;
+    shell.exec(compileCommand,{async:true}).stdout;
     shell.exec('echo upload successfully',{async:true}).stdout;
 
     var code = Blockly.Kidtronics.workspaceToCode(workspace);
@@ -16,8 +19,10 @@ $("#compile").click(function(){
     fs.writeFile('./arduino_files/test_led/test_led.ino', code); 
 });
 
+
+
+//TODO: Windows and linux version needed
 $("#devices_list").click(function(){
-    console.log("hi");
     $('#devices_list').empty();
     var files = shell.ls('-A','/dev');
     var count = 0;
@@ -26,7 +31,7 @@ $("#devices_list").click(function(){
         if(subStr === 'cu.usb')
         {
             count++;
-            $('#devices_list').append('<option>'+files[i]+'</option>');
+            $('#devices_list').append('<option value='+files[i]+'>'+files[i]+'</option>');
         }
     }
     if(count == 0){
